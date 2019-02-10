aa6b6779c3312bae908bd518070aa9609ed811cb
Shuvoneel <mhshuvo29@gmail.com>
2019-02-10 18:47:30 +0600

    binary search, chapter 25

---
+++ BinarySearchTree/src/binarysearchtree/chapter_25/AbstractTree.java
@@ -0,0 +1,20 @@
+package binarysearchtree.chapter_25;
+
+public abstract class AbstractTree<E> implements Tree<E> {
+  /** Inorder traversal from the root*/
+  public void inorder() {
+  }
+
+  /** Postorder traversal from the root */
+  public void postorder() {
+  }
+
+  /** Preorder traversal from the root */
+  public void preorder() {
+  }
+
+  /** Return true if the tree is empty */
+  public boolean isEmpty() {
+      return isEmpty();
+  }
+}
