19d995e24cb86dcf20df2b27c259c89862315479
Shuvoneel <mhshuvo29@gmail.com>
2019-02-12 18:55:50 +0600

    Hashing, chapter 27

---
+++ BinarySearchTree/src/feb_10/chapter_25/AbstractTree.java
@@ -0,0 +1,22 @@
+package feb_10.chapter_25;
+
+import feb_10.chapter_25.Tree;
+
+public abstract class AbstractTree<E> implements Tree<E> {
+  /** Inorder traversal from the root*/
+  public void inorder() {
+  }
+
+  /** Postorder traversal from the root */
+  public void postorder() {
+  }
+
+  /** Preorder traversal from the root */
+  public void preorder() {
+  }
+
+  /** Return true if the tree is empty */
+  public boolean isEmpty() {
+      return isEmpty();
+  }
+}
---
+++ BinarySearchTree/src/feb_10/chapter_25/Node.java
@@ -0,0 +1,12 @@
+package feb_10.chapter_25;
+class Node 
+{ 
+    int key; 
+    Node left, right; 
+  
+    public Node(int item) 
+    { 
+        key = item; 
+        left = right = null; 
+    } 
+} 
